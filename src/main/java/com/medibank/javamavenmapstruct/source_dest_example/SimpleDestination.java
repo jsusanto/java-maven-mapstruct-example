47d5a836c709cccca83819e40d9107ea9e445837
Jeffry Susanto <jeffry.susanto@medibank.com.au>
2024-03-04 10:00:21 +1100

    Remove incorrect commit file as it's automatically created when building.

--- src/main/java/com/medibank/javamavenmapstruct/source_dest_example/SimpleDestination.java
+++ src/main/java/com/medibank/javamavenmapstruct/source_dest_example/SimpleDestination.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.medibank.javamavenmapstruct.source_dest_example;
 
-/**
- *
- * @author 606041
- */
 public class SimpleDestination {
     private String name;
 
--- src/main/java/com/medibank/javamavenmapstruct/source_dest_example/SimpleSourceDestinationMapper.java
+++ src/main/java/com/medibank/javamavenmapstruct/source_dest_example/SimpleSourceDestinationMapper.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.medibank.javamavenmapstruct.source_dest_example;
 
 import org.mapstruct.Mapper;
-/**
- *
- * @author 606041
- */
+
 @Mapper
 public interface SimpleSourceDestinationMapper {
     SimpleDestination sourceToDestination(SimpleSource source);
